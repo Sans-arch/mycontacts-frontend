ce844994f9db216a62c179ce6c2d334ade188344
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-03 13:13:57 -0300

    Create Card Component

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -3,9 +3,14 @@ export default {
     background: '#F6F5FC',
     primary: {
       main: '#5061FC',
-      lighter: '#E0E3FF;',
+      lighter: '#E0E3FF',
       light: '#6674F4',
-      dark: '#3346F0;',
+      dark: '#3346F0',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+      100: '#E6E6E6',
     },
   },
 };
