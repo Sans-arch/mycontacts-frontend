4802072f0db1cfb446e7c6ab2ae33cd90c855e67
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-03 22:00:46 -0300

    Create Home page and refactor Header component

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,27 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   margin-top: 66px;
+  margin-bottom: 48px;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-export const InputSearchContainer = styled.div`
-  margin-top: 48px;
-  width: 100%;
-
-  input {
-    border: none;
-    border-radius: 25px;
-    width: 100%;
-    height: 50px;
-    background: #fff;
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
-    outline: 0;
-    padding: 0 16px;
-
-    &::placeholder {
-      color: #BCBCBC;
-    }
-  }
 `;
