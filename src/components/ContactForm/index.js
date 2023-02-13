762cb39d83654409ca359dfef1bb61fc422ffa3f
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-13 08:58:24 -0300

    Work with Controlled Components

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Form, ButtonContainer } from './styles';
@@ -7,11 +8,31 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
+/*
+  Formulários dentro do ReactJS:
+  Adicionando event listeners.
+  1° forma) Controlled components:
+    São os campos do formulário onde a responsabilidade de controlar o valor desse componente
+    no JS e na DOM são do React, então o React é o responsável por gerenciar os campos do form.
+    React responsável por gerenciar o dado e a renderização.
+
+  2° forma) Uncontrolled components:
+    Responsabilidade de fazer gerenciamento não é mais do React, ele deixa com a DOM, o React
+    não vai ter acesso aos componentes, não vai ter acesso ao que vai ser renderizado.
+*/
 export default function ContactForm({ buttonLabel }) {
+  // Atualiza estado e remonta o componente inteiro (re-renderiza), a cada alteração no input vai
+  // renderizar todo o compontente de novo!
+  const [name, setName] = useState('');
+
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          value={name}
+          placeholder="Nome"
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup
